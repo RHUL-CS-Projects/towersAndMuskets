b462f63e2550652df0aa837945cc4fb2b3e98516
zyvb031 <zyvb031@029d1f5e-ba45-47f3-9ba4-d89dc58e0cf9>
2015-10-31 14:47:58 +0000

    Set up Irrlicht to correctly compile and link

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,6 +2,7 @@
 #include <Server.h>
 #include <Client.h>
 #include <string>
+#include <irrlicht/irrlicht.h>
 
 #include <signal.h>
 #include <stdlib.h>
@@ -9,6 +10,12 @@
 #include <unistd.h>
 
 using namespace std;
+using namespace irr;
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
 
 Server server;
 Client client;
@@ -19,7 +26,7 @@ void signalExit(int s) {
 }
 
 int main() {
-	struct sigaction sigIntHandler;
+	/*struct sigaction sigIntHandler;
 	sigIntHandler.sa_handler = signalExit;
 	sigemptyset(&sigIntHandler.sa_mask);
 	sigIntHandler.sa_flags = 0;
@@ -39,7 +46,30 @@ int main() {
 		cin >> input;
 		
 		client.connectToServer(input);
+	}*/
+	
+	IrrlichtDevice* device = createDevice(video::EDT_SOFTWARE, dimension2d<u32>(640, 480), 16, false, false, false, 0);
+	
+	if (!device)
+		return 1;
+	
+	device->setWindowCaption(L"Tower Defense");
+	IVideoDriver* driver = device->getVideoDriver();
+	ISceneManager* smgr = device->getSceneManager();
+	IGUIEnvironment* guienv = device->getGUIEnvironment();
+	
+	guienv->addStaticText(L"Hello world!", rect<s32>(10,10,260,22), true);
+	guienv->addButton(rect<s32>(10,40,140,80),0, -1, L"Click me");
+	
+	while (device->run()) {
+		driver->beginScene(true, true, SColor(255,100,101,140));
+		
+		guienv->drawAll();
+		
+		driver->endScene();
 	}
+	
+	device->drop();
 	
 	return 0;
 }
