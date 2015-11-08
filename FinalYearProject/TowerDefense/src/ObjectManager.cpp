e8ba32e16857bbc1c244df978c14047e75eb4a35
zyvb031 <zyvb031@029d1f5e-ba45-47f3-9ba4-d89dc58e0cf9>
2015-11-08 22:18:06 +0000

    Object manager functionality is almost complete

--- FinalYearProject/TowerDefense/src/ObjectManager.cpp
+++ FinalYearProject/TowerDefense/src/ObjectManager.cpp
@@ -9,7 +9,6 @@ unordered_map<int, list<GameComponent>> ObjectManager::objectComponents;
 unordered_map<string, list<int>> ObjectManager::componentObjects;
 list<ComponentSystem> ObjectManager::systems;
 
-
 /**
  * Get the total number of objects currently in the game.
  */
@@ -22,6 +21,7 @@ int ObjectManager::getObjectCount() {
  */
 int ObjectManager::createObject() {
 	int newObjID = objectID;
+	objectComponents.insert(make_pair(newObjID, list<GameComponent>()));
 	
 	objectID++;
 	objectCount++;
@@ -34,6 +34,11 @@ int ObjectManager::createObject() {
  * id from the object map.
  */
 void ObjectManager::destroyObject ( int id ) {
+	if (!objectExists(id)) {
+		cerr << "Destroy Object Failed: Object with ID=" << id << " does not exist!" << endl;
+		return;
+	}
+	
 	list<GameComponent> components = getObjectComponents(id);
 	
 	for (GameComponent gc : components) {
@@ -48,6 +53,11 @@ void ObjectManager::destroyObject ( int id ) {
  * Check if a given object has a specific component attached to it.
  */
 bool ObjectManager::objectHasComponent ( int id, string componentName ) {
+	if (!objectExists(id)) {
+		cerr << "Object Has Component Failed: Object with ID=" << id << " does not exist!" << endl;
+		return false;
+	}
+	
 	list<GameComponent> components = getObjectComponents(id);
 	
 	for (GameComponent gc : components) {
@@ -61,7 +71,12 @@ bool ObjectManager::objectHasComponent ( int id, string componentName ) {
 /**
  * Get a specific component attached to a given object id.
  */
-GameComponent ObjectManager::getObjectComponent ( int id, string componentName ) {
+GameComponent ObjectManager::getObjectComponent( int id, string componentName ) {
+	if (!objectExists(id)) {
+		cerr << "Get Object Component Failed: Object with ID=" << id << " does not exist!" << endl;
+		return GameComponent("EmptyComponent");
+	}
+	
 	list<GameComponent> components = getObjectComponents(id);
 	
 	for (GameComponent gc : components) {
@@ -69,14 +84,19 @@ GameComponent ObjectManager::getObjectComponent ( int id, string componentName )
 			return gc;
 	}
 	
-	return GameComponent();
+	return GameComponent("EmptyComponent");
 }
 
 /**
  * Get a list of all components attached to a game object.
  */
 list< GameComponent > ObjectManager::getObjectComponents ( int id ) {
-	return ((list<GameComponent>)objectComponents.at(id));
+	if (objectExists(id)) {
+		return objectComponents.at(id);
+	}
+	
+	cerr << "Get Object Components Failed: Object with ID=" << id << " does not exist!" << endl;
+	return list<GameComponent>();
 }
 
 /**
@@ -91,24 +111,43 @@ list< int > ObjectManager::getObjectsWithComponent ( string componentName ) {
  * Attach a component to a game object.
  */
 void ObjectManager::attachComponent ( int id, GameComponent component ) {
-	list<GameComponent> components = getObjectComponents(id);
-	components.push_back(component);
+	if (!objectExists(id)) {
+		cerr << "Attach Component Failed: Object with ID=" << id << " does not exist!" << endl;
+		return;
+	}
 	
-	objectComponents.insert(make_pair(id, components));
+	list<GameComponent>* components = &objectComponents.at(id);
+	components->push_back(component);
+	
+	//objectComponents.insert(make_pair(id, components));
 	
 	if (componentObjects.count(component.getName()) == 0) {
 		componentObjects.insert(make_pair(component.getName(), list<int>()));
 	}
 	
-	((list<int>)componentObjects.at(component.getName())).push_back(id);
+	(&componentObjects.at(component.getName()))->push_back(id);
 }
 
 /**
  * Remove a component a given object.
  */
 void ObjectManager::detachComponent ( int id, string componentName ) {
-	list<int> objectIDs = ((list<int>)componentObjects.at(componentName));
-	objectIDs.remove(id);
+	if (!objectExists(id)) {
+		cerr << "Detach Component Failed: Object with ID=" << id << " does not exist!" << endl;
+		return;
+	}
+	
+	list<int>* objectIDs = &componentObjects.at(componentName);
+	objectIDs->remove(id);
+	
+	list<GameComponent>* components = &objectComponents.at(id);
+	
+	for (GameComponent gc : *components) {
+		if (gc.getName() == componentName) {
+			components->remove(gc);
+			break;
+		}
+	}
 }
 
 /**
@@ -117,6 +156,63 @@ void ObjectManager::detachComponent ( int id, string componentName ) {
 void ObjectManager::updateSystems ( float timestep ) {
 	// system::update(); etc.
 }
+
+/**
+ * Check if there is an object with the given id currently
+ * in the game.
+ */
+bool ObjectManager::objectExists ( int id ) {
+	return objectComponents.find(id) != objectComponents.end();
+}
+
+
+/**
+ * Print information about a game object's components for debugging purposes.
+ */
+void ObjectManager::printGameObject ( int id ) {
+	if (!objectExists(id)) {
+		cerr << "Print Object Failed: Object with ID=" << id << " does not exist!" << endl;
+		return;
+	}
+	
+	list<GameComponent> components = getObjectComponents(id);
+	if (components.size() == 0) {
+		cout << "Object ID=" << id << " has NO components" << endl;
+		return;
+	}
+	
+	cout << "Object ID=" << id << " has components:" << endl;
+	
+	for (GameComponent gc : components) {
+		cout << gc.getName() << endl;
+	}
+	
+	cout << endl;
+}
+
+/**
+ * Print the IDs of all game objects that have a specific component attached.
+ */
+void ObjectManager::printObjectsWithComponent ( string componentName ) {
+	list<int> ids = getObjectsWithComponent(componentName);
+	if (ids.size() == 0) {
+		cout << "NO components have " << componentName << " attached" << endl;
+		return;
+	}
+	
+	cout << "Object IDs with " << componentName << " attached:" << endl;
+	for (int i : ids) {
+		cout << i << ", ";
+	}
+	
+	cout << endl << endl;
+}
+
+
+
+
+
+
 
 
 
